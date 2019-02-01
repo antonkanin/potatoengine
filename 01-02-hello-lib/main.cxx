44ec0e70f651dfec02063cd667fee881e2994d1b
antonkanin <antony.kanin@gmail.com>
2019-02-01 16:54:14 +0300

    linking dll function dynamically during the runtime

--- 01-02-hello-lib/main.cxx
+++ 01-02-hello-lib/main.cxx
@@ -1,11 +1,32 @@
-#include "hellolib.h"
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+typedef bool (*f_hello)(const std::string&);
 
 int main(int argc, char* argv[])
 {
-    if (hello("Anton"))
+    HINSTANCE helloDll = LoadLibrary("hellolib.dll");
+    if (helloDll == nullptr)
     {
-        return EXIT_SUCCESS;
+        std::cout << "Cannot locate hellolib.dll" << std::endl;
+        return EXIT_FAILURE;
     }
-    
+    else
+    {
+        f_hello hello = (f_hello)GetProcAddress(helloDll, "hello");
+        if (!hello)
+        {
+            if (hello("Anton"))
+            {
+                return EXIT_SUCCESS;
+            }
+        }
+        else
+        {
+            // TODO add error handling if we did not find a function in the dll
+        }
+    }
+
     return EXIT_FAILURE;
 }
