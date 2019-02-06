6dfe407228aca35d523b830cbcd067d2778addb3
Anton Kanin <antony.kanin@gmail.com>
2019-02-07 01:45:48 +0300

    adding sdl loop app - work in progress

--- 03-01-sdl-loop/main.cxx
+++ 03-01-sdl-loop/main.cxx
@@ -1,6 +1,37 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+void process_input(const SDL_Event& event)
+{
+    using namespace std;
+
+    switch (event.key.keysym.sym)
+    {
+        case SDLK_UP:
+        {
+            if (event.key.type == SDL_KEYUP)
+            {
+                cout << "UP lifted" << '\n';
+            }
+            else
+            {
+                cout << "UP pressed" << '\n';
+            }
+            break;
+        }
+        case SDLK_DOWN:
+        {
+            cout << "DOWN" << '\n';
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     using namespace std;
@@ -12,30 +43,57 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    const SDL_Window* window =
+    // TODO fix the warning "Warning:(47, 43) Clang-Tidy: Use of a signed
+    // integer operand with a binary bitwise operator"
+    SDL_Window* window =
         SDL_CreateWindow("SDL Game Loop", SDL_WINDOWPOS_CENTERED,
                          SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 
     if (window == nullptr)
     {
-        // SDL_Log("Error: failed to ")
-
+        SDL_Log("Error: failed to SDL window %s", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
     }
-
-
-
-
-    // main game loop
-    SDL_Event event;
 
     bool game_running = true;
 
     while (game_running)
     {
+        // main game loop
+        SDL_Event event;
         while (SDL_PollEvent(&event))
         {
+            switch (event.type)
+            {
+                case SDL_KEYDOWN:
+                {
+                    process_input(event);
+                    break;
+                }
+
+                case SDL_KEYUP:
+                {
+                    process_input(event);
+                    break;
+                }
+
+                case SDL_QUIT:
+                {
+                    game_running = false;
+                }
+
+                default:
+                {
+                    break;
+                }
+            }
         }
     }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
