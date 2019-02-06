25a2405acd2bcb03db821ec1386a07fe289ace78
Anton Kanin <antony.kanin@gmail.com>
2019-02-06 18:29:11 +0300

    static build works now

--- 02-02-sdl-static/main.cxx
+++ 02-02-sdl-static/main.cxx
@@ -1,15 +1,17 @@
-#include <iostream>
 #include <SDL2/SDL_version.h>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
     using namespace std;
 
-    SDL_version version = { 0, 0, 0};
+    SDL_version version = { 0, 0, 0 };
 
     SDL_GetVersion(&version);
 
-    cout << "SDL version: " << version.minor << "." << version.minor << "." << version.patch << "\n";
+    cout << "SDL version: " << static_cast<int>(version.major) << "."
+         << static_cast<int>(version.minor) << "."
+         << static_cast<int>(version.patch) << "\n";
 
     return EXIT_SUCCESS;
 }
