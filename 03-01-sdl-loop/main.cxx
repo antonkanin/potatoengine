b9cdf651851b3fe182eceb198cdd0dc9fce75b94
Anton Kanin <antony.kanin@gmail.com>
2019-02-06 23:28:43 +0300

    adding sdl loop app - work in progress

--- 03-01-sdl-loop/main.cxx
+++ 03-01-sdl-loop/main.cxx
@@ -1,32 +1,41 @@
+#include <SDL2/SDL.h>
 #include <iostream>
-#include <SDL2/SDL_events.h>
 
-int main(int argc, int* argv[])
+int main(int argc, char* argv[])
 {
     using namespace std;
 
-    cout << "Test" << endl;
-
-    bool game_running = true;
-
-    // main game loop
-    SDL_Event event;
-
-    while(game_running)
+    const int init_result = SDL_Init(SDL_INIT_EVERYTHING);
+    if (init_result != 0)
     {
-        while (SDL_PollEvent(event))
-        {
+        SDL_Log("Error: failed to initialize SDL %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
-        }
+    const SDL_Window* window =
+        SDL_CreateWindow("SDL Game Loop", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 
-
-
+    if (window == nullptr)
+    {
+        // SDL_Log("Error: failed to ")
 
     }
 
 
+
+
+    // main game loop
+    SDL_Event event;
+
+    bool game_running = true;
+
+    while (game_running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+        }
+    }
+
     return EXIT_SUCCESS;
 }
-
-
-
