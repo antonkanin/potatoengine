1a84796d279cadce1a4ce0c0ff85515ddec56022
Anton Kanin <antony.kanin@gmail.com>
2019-03-07 22:17:12 +0300

    shaders code clean up

--- 04-01-opengl/engine/src/renderer/shaders/generic_shader.frag
+++ 04-01-opengl/engine/src/renderer/shaders/generic_shader.frag
@@ -15,18 +15,5 @@ void main()
     vec3 norm = normalize(vec3(v_normal));
     vec3 light_dir = normalize(vec3(u_light_pos) - vec3(v_position));
     float diff = max(dot(norm, light_dir), 0.1) + 0.1;
-    gl_FragColor = vec4(v_color, 1.0) * diff;
-
-
-    //vec3 t = normalize(u_light_pos);
-    //gl_FragColor = vec4(t.x, 0.0, 0.0, 1.0);
-    //gl_FragColor = vec4(v_color, 1.0) * diff;
-
-    //vec3 l = normalize(v_light_position.xyz);
-
-    //gl_FragColor = vec4(diff, 0.0, 0.0, 1.0);
-    //gl_FragColor = vec4(l.x, 0.0, 0.0, 1.0);
-
-    //gl_FragColor = texture2D(texture, v_tex_coord) * vec4(v_color, 1.0) * diff;
-
+    gl_FragColor = texture2D(texture, v_tex_coord) * vec4(v_color, 1.0) * diff;
 }
--- 04-01-opengl/game/game.cpp
+++ 04-01-opengl/game/game.cpp
@@ -23,16 +23,6 @@ int main(int argc, char* argv[])
             ->set_position({ -3.f, 0.f, 0.f });
             //->set_scale({0.1f, 0.1f, 0.1f});
 
-    engine->add_object<space_ship>()->
-            set_position({ -4.f, 5.f, 0.f });
-
-    engine->add_object<space_ship>()->
-            set_position({ -4.f, 5.f, -5.f });
-
-    engine->add_object<space_ship>()->
-            set_position({ -4.f, 5.f, -5.f });
-
-
     engine->add_object<light_controller>()
             ->set_position({ 3.f, 0.f, 0.f });
             //->set_scale({0.2f, 0.2f, 0.2f});
@@ -42,7 +32,7 @@ int main(int argc, char* argv[])
 
     engine->add_object<camera_keyboard_controller>();
 
-    engine->add_object<vehicle>();
+    engine->add_object<vehicle>()->set_position({ 10.f, 0.f, 6.f });
 
     engine->add_object<show_fps>();
 
