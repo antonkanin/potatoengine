f9ea31a77df2e3b8ed020bf3c9985312b8c79b49
antonkanin <antony.kanin@gmail.com>
2019-04-07 01:02:12 +0300

    Win64 compilation works now - forcing glm::mat4 identity matrix

--- 04-01-opengl/game/game.cpp
+++ 04-01-opengl/game/game.cpp
@@ -41,34 +41,16 @@ int main(int argc, char* argv[])
 
     pt::load_scene(*engine, "scenes/level01.yaml");
 
-    //pt::save_scene(*engine, "scenes/level02.yaml");
-
-//    engine->add_object<space_ship>("Space ship 1")
-//            ->set_position({ -3.f, 3.f, -5.f })
-//            ->add_body(true);
-//
-//    engine->add_object<space_ship>("Space ship 2")
-//            ->set_position({ -3.f, -5.f, -5.f })
-//            ->set_scale({2.f, .1f, 2.f})
-//            ->add_body(false);
-//
-//    engine->add_object<space_ship>()
-//            ->set_position({ -3.f, 4.f, 0.f });
-//
-//    engine->add_object<space_ship>()
-//            ->set_position({ -3.f, 4.f, 5.f });
-
-
-//    engine->add_object<light_controller>("Light")
-//            ->set_position({ 3.f, 0.f, 0.f });
+    engine->add_object<light_controller>("Light")
+            ->set_position({ 3.f, 0.f, 0.f });
 
 
 
-//    engine->add_object<camera_gui_controller>("Camera GUI")->
-//            set_position({ 0.f, 0.f, 6.f });
-//
-;    engine->add_object<camera_keyboard_controller>("Keyboard Controller");
-//
+    engine->add_object<camera_gui_controller>("Camera GUI")->
+            set_position({ 0.f, 0.f, 6.f });
+
+    engine->add_object<camera_keyboard_controller>("Keyboard Controller");
+
     engine->add_object<player>("Player");
 
     engine->add_object<enemies_spawner>("Enemies spawner");
