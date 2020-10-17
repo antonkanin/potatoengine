9c8cfe03381ddc2b266c2320bb9c87fabf9d11dd
Anton Kanin <anton.kanin@ephl.by>
2020-10-17 18:20:11 +0300

    adding model description

--- game/game.hpp
+++ game/game.hpp
@@ -37,7 +37,7 @@ void runGame()
     PT_REGISTER(engine, cube);
     PT_REGISTER(engine, enemy);
 
-    pt::load_scene(*engine, "scenes/level01.yaml");
+    // pt::load_scene(*engine, "scenes/level01.yaml");
 
     engine->add_object<light_controller>("Light")->set_position(
         { 1.f, 2.f, 6.f });
